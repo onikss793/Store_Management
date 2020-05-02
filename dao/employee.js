11becfd17da9bd6c7053fafa161d3367c32ceab9
Woongjo Yoo <onikss793@gmail.com>
2020-05-02 13:51:53 +0900

    [UPDATE] 휴가 중인 점원 목록 표시

--- dao/employee.js
+++ dao/employee.js
@@ -1,7 +1,26 @@
-const { Employee, Vacation } = require('../models');
+const { Employee, Vacation } = require('../models'),
+	{ QueryTypes } = require('sequelize'),
+	Sequelize = require('../database');
 
 const selectEmployeesByStoreId = (store_id) => {
-	return Employee.findAll({ where: { store_id } });
+	return Sequelize.query(`
+		SELECT
+			e.id AS id,
+			e.employee_name AS employee_name,
+			e.enrolled_in AS enrolled_in,
+			e.store_id AS store_id,
+				CASE WHEN v.start_at <= CURRENT_TIMESTAMP
+					AND v.finish_at >= CURRENT_TIMESTAMP
+					THEN 'true'
+				ELSE 'false'
+				END AS vacation
+		FROM
+			employees AS e
+		LEFT JOIN vacations AS v ON e.id = v.employee_id
+		WHERE
+			e.deleted_at IS NULL AND e.store_id = ${ store_id }
+	`, { types: QueryTypes.SELECT, model: Employee });
+	// return Employee.findAll({ where: { store_id } });
 };
 
 const insertEmployee = (data) => {
